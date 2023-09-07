a2a5ad54f83a83daa4dfaa66ecb0abd6a6126a0f
Krzysztof Wielgosz <14099566+frankii91@users.noreply.github.com>
2023-09-07 16:05:56 +0200

    first

--- src/readme.js
+++ src/readme.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const parentDir = path.join(__dirname, '..');
-const filePath = path.join(parentDir, 'README.MD');
+const filePath = path.join(parentDir, 'README-HOWTO.MD');
 
 import showdown from 'showdown';
 const { Converter } = showdown;
